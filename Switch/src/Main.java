1f5bc410442a1410a537589db9af1bebd6d25615
Hoa Truong <huynhhoapy97@gmail.com>
2023-07-07 14:22:36 +0700

    Code switch function

--- Switch/src/Main.java
+++ Switch/src/Main.java
@@ -2,6 +2,33 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello switch");
+        String day = "Sunday1";
+
+        switch (day) {
+            case "Sunday":
+            case "Sunday1":
+                System.out.println("It is Sunday");
+                //break;
+            case "Monday":
+                System.out.println("It is Monday");
+                break;
+            case "Tuesday":
+                System.out.println("It is Tuesday");
+                break;
+            case "Wednesday":
+                System.out.println("It is Wednesday");
+                break;
+            case "Thursday":
+                System.out.println("It is Thursday");
+                break;
+            case "Friday":
+                System.out.println("It is Friday");
+                break;
+            case "Saturday":
+                System.out.println("It is Saturday");
+                break;
+            default:
+                System.out.println("It is not a day");
+        }
     }
 }
