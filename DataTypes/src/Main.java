cff9fd1637cbf878859ab14d628268e344adf3d4
Hoa Truong <huynhhoapy97@gmail.com>
2023-07-12 15:17:53 +0700

    Data Types in Java

---
+++ DataTypes/src/Main.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        float f = 1.5f;
+        Float F = 1.5f;
+
+        double x = 10.0/2;
+        System.out.println("x = " + x);
+
+        double y = (int)10.0/2;
+        System.out.println("y = " + y);
+    }
+}
